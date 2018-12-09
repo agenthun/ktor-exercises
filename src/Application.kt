6614b5e8a717a72b23981cb90d8ba2928c97cb3c
agenthun <hun333@126.com>
2018-12-09 13:58:20 +0800

    [ex13] JWT and JWK authentication

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,13 @@
 package com.agenthun
 
+import com.auth0.jwk.JwkProviderBuilder
 import io.ktor.application.Application
 import io.ktor.application.ApplicationCallPipeline
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.auth.*
+import io.ktor.auth.jwt.JWTPrincipal
+import io.ktor.auth.jwt.jwt
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.StatusPages
@@ -37,6 +40,7 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.security.MessageDigest
 import java.text.DateFormat
+import java.util.concurrent.TimeUnit
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -200,6 +204,26 @@ fun Application.module(testing: Boolean = false) {
     intercept(ApplicationCallPipeline.Call) {
         if (call.request.uri == "/") {
 //            call.respondText("Test String")
+        }
+    }
+
+    val jwtIssuer = environment.config.property("jwt.domain").getString()
+    val jwtAudience = environment.config.property("jwt.audience").getString()
+    val jwtRealm = environment.config.property("jwt.realm").getString()
+
+    val jwkIssuer = "https://jwt-provider-domain/"
+    val jwkRealm = "ktor jwt auth test"
+    val jwkProvider = JwkProviderBuilder(jwkIssuer)
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+    authentication {
+        jwt {
+            realm = jwtRealm
+            verifier(jwkProvider, jwkIssuer)
+            validate { credentials ->
+                if (credentials.payload.audience.contains(jwtAudience)) JWTPrincipal(credentials.payload) else null
+            }
         }
     }
 //    val exec = Executors.newCachedThreadPool()
